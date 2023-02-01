d088b7b9a5bb128fb5781434a192c5ce0f6ee572
Guilherme Oliveira <g.h.p.o3615@gmail.com>
2023-01-31 22:03:29 -0300

    criando projeto mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Talita TE AMO S2</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff'
+  }
+});
