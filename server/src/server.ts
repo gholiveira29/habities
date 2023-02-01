4e6f6799dbd87e7e3f0ce57437b7074be60bcdf5
Guilherme Oliveira <g.h.p.o3615@gmail.com>
2023-01-31 21:30:45 -0300

    criando projeto web

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-    return "Hello";
+app.register(cors);
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany();
+    return habits;
 });
 
 app.listen({
