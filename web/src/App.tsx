618d2228840fb79c0136de61a07abf6132e21741
Guilherme Oliveira <g.h.p.o3615@gmail.com>
2023-02-02 19:28:56 -0300

    iniciando o layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import Habit from "./components/Habit"
 import './styles/global.css';
+import logoImage from './assets/logo.svg';
 
 function App() {
   return (
-    <div>
-      <Habit completed={3} />
+    <div className="v-sscren h-screen flex justify-center items-center">
+      <div className="h-full max-w-5xl px-6 flex flex-col gap-16">
+        <img src={logoImage} alt="habits" ></img>
+      </div>
     </div>
   )
 }
