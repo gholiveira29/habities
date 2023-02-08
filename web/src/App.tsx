84ab021eacd6c9414f2933789934011ece8ca16c
Guilherme Oliveira <g.h.p.o3615@gmail.com>
2023-02-08 19:18:12 -0300

    criando componetes no web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
-import Habit from "./components/Habit"
+import Header from "./components/Header";
+import SummarryTable from "./components/SummaryTable";
 import './styles/global.css';
-import logoImage from './assets/logo.svg';
+
 
 function App() {
   return (
-    <div className="v-sscren h-screen flex justify-center items-center">
-      <div className="h-full max-w-5xl px-6 flex flex-col gap-16">
-        <img src={logoImage} alt="habits" ></img>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummarryTable />
       </div>
     </div>
   )
---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,10 @@
+// interface HabitProps {
+//     completed: number;
+// }
+
+const HabitDay = () => {
+    return (
+        <div className="w-10 h-10 bg-zinc-700 boder-2 border-zinc-600 rounded-lg" />
+    )
+}
+export default HabitDay;
